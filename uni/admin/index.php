16b01f3be40ddfd262a3aacc873b8582911c6bd4
ediibraimi <ediibraimi9@gmail.com>
2024-05-26 01:58:03 +0200

    cxncfv

--- uni/admin/index.php
+++ uni/admin/index.php
@@ -1,3 +1,36 @@
+<?php
+include_once('../config.php');
+session_start();
+$queary = "SELECT ifnull(SUM(price),0) total FROM memberships;";
+
+//////
+$stmt = $conn->prepare($queary);
+$stmt->execute();
+$row = $stmt->fetch();
+$totalPrice = $row['total'];
+/////
+//////
+$queryteachers = "SELECT count(*) numri FROM `teacher_profile`;";
+$stmt1 = $conn->prepare($queryteachers);
+$stmt1->execute();
+$row1 = $stmt1->fetch();
+$totalteacher = $row1['numri'];
+/////
+/////
+$queryusers = "SELECT count(*) users FROM `teacher_profile`;";
+$stmt2 = $conn->prepare($queryusers);
+$stmt2->execute();
+$row2 = $stmt2->fetch();
+$totalusers = $row2['users'];
+/////
+?>
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,49 +44,50 @@
 	<meta name="keywords" content="" />
 	<meta name="author" content="" />
 	<meta name="robots" content="" />
-	
+
 	<!-- DESCRIPTION -->
 	<meta name="description" content="EduChamp : Education HTML Template" />
-	
+
 	<!-- OG -->
 	<meta property="og:title" content="EduChamp : Education HTML Template" />
 	<meta property="og:description" content="EduChamp : Education HTML Template" />
 	<meta property="og:image" content="" />
 	<meta name="format-detection" content="telephone=no">
-	
+
 	<!-- FAVICONS ICON ============================================= -->
 	<link rel="icon" href="../error-404.php" type="image/x-icon" />
 	<link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.png" />
-	
+
 	<!-- PAGE TITLE HERE ============================================= -->
 	<title>EduChamp : Education HTML Template </title>
-	
+
 	<!-- MOBILE SPECIFIC ============================================= -->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	
+
 	<!--[if lt IE 9]>
 	<script src="assets/js/html5shiv.min.js"></script>
 	<script src="assets/js/respond.min.js"></script>
 	<![endif]-->
-	
+
 	<!-- All PLUGINS CSS ============================================= -->
 	<link rel="stylesheet" type="text/css" href="assets/css/assets.css">
 	<link rel="stylesheet" type="text/css" href="assets/vendors/calendar/fullcalendar.css">
-	
+
 	<!-- TYPOGRAPHY ============================================= -->
 	<link rel="stylesheet" type="text/css" href="assets/css/typography.css">
-	
+
 	<!-- SHORTCODES ============================================= -->
 	<link rel="stylesheet" type="text/css" href="assets/css/shortcodes/shortcodes.css">
-	
+
 	<!-- STYLESHEETS ============================================= -->
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/dashboard.css">
 	<link class="skin" rel="stylesheet" type="text/css" href="assets/css/color/color-1.css">
-	
+
 </head>
+
 <body class="ttr-opened-sidebar ttr-pinned-sidebar">
-	
+
 	<!-- header start -->
 	<header class="ttr-header">
 		<div class="ttr-header-wrapper">
@@ -64,7 +98,7 @@
 			</div>
 			<!--sidebar menu toggler end -->
 			<!--logo start -->
-			
+
 			<!--logo end -->
 			<div class="ttr-header-menu">
 				<!-- header left menu start -->
@@ -170,7 +204,7 @@
 							<ul>
 								<li><a href="teacherDashboard.php">My profile</a></li>
 								<li><a href="list-view-calendar.php">Activity</a></li>
-								
+
 								<li><a href="../login.php">Logout</a></li>
 							</ul>
 						</div>
@@ -227,7 +261,7 @@
 	<div class="ttr-sidebar">
 		<div class="ttr-sidebar-wrapper content-scroll">
 			<!-- side menu logo start -->
-			
+
 			<!-- side menu logo end -->
 			<!-- sidebar menu start -->
 			<nav class="ttr-sidebar-navi">
@@ -235,65 +269,65 @@
 					<li>
 						<a href="index.php" class="ttr-material-button">
 							<span class="ttr-icon"><i class="ti-home"></i></span>
-		                	<span class="ttr-label">Dashborad</span>
-		                </a>
-		            </li>
+							<span class="ttr-label">Dashborad</span>
+						</a>
+					</li>
 					<li>
 						<a href="courses.php" class="ttr-material-button">
 							<span class="ttr-icon"><i class="ti-book"></i></span>
-		                	<span class="ttr-label">Courses</span>
-		                </a>
-		            </li>
-					
+							<span class="ttr-label">Courses</span>
+						</a>
+					</li>
+
 					<li>
 						<a href="#" class="ttr-material-button">
 							<span class="ttr-icon"><i class="ti-calendar"></i></span>
-		                	<span class="ttr-label">Calendar</span>
-		                	<span class="ttr-arrow-icon"><i class="fa fa-angle-down"></i></span>
-		                </a>
-		                <ul>
-		                	<li>
-		                		<a href="basic-calendar.php" class="ttr-material-button"><span class="ttr-label">Basic Calendar</span></a>
-		                	</li>
-		                	<li>
-		                		<a href="list-view-calendar.php" class="ttr-material-button"><span class="ttr-label">List View</span></a>
-		                	</li>
-		                </ul>
-		            </li>
+							<span class="ttr-label">Calendar</span>
+							<span class="ttr-arrow-icon"><i class="fa fa-angle-down"></i></span>
+						</a>
+						<ul>
+							<li>
+								<a href="basic-calendar.php" class="ttr-material-button"><span class="ttr-label">Basic Calendar</span></a>
+							</li>
+							<li>
+								<a href="list-view-calendar.php" class="ttr-material-button"><span class="ttr-label">List View</span></a>
+							</li>
+						</ul>
+					</li>
 					<li>
 						<a href="bookmark.php" class="ttr-material-button">
 							<span class="ttr-icon"><i class="ti-bookmark-alt"></i></span>
-		                	<span class="ttr-label">Bookmarks</span>
-		                </a>
-		            </li>
+							<span class="ttr-label">Bookmarks</span>
+						</a>
+					</li>
 					<li>
 						<a href="review.php" class="ttr-material-button">
 							<span class="ttr-icon"><i class="ti-comments"></i></span>
-		                	<span class="ttr-label">Review</span>
-		                </a>
-		            </li>
+							<span class="ttr-label">Review</span>
+						</a>
+					</li>
 					<li>
 						<a href="add-listing.php" class="ttr-material-button">
 							<span class="ttr-icon"><i class="ti-layout-accordion-list"></i></span>
-		                	<span class="ttr-label"></span>
-		                </a>
-		            </li>
+							<span class="ttr-label"></span>
+						</a>
+					</li>
 					<li>
 						<a href="#" class="ttr-material-button">
 							<span class="ttr-icon"><i class="ti-user"></i></span>
-		                	<span class="ttr-label">My Profile</span>
-		                	<span class="ttr-arrow-icon"><i class="fa fa-angle-down"></i></span>
-		                </a>
-		                <ul>
-		                	<li>
-		                		<a href="teacherDashboard.php" class="ttr-material-button"><span class="ttr-label">Teacher Dashboard</span></a>
-		                	</li>
-		                	<li>
-		                		<a href="teacher-profile.php" class="ttr-material-button"><span class="ttr-label">Teacher Profile</span></a>
-		                	</li>
-		                </ul>
-		            </li>
-		            <li class="ttr-seperate"></li>
+							<span class="ttr-label">My Profile</span>
+							<span class="ttr-arrow-icon"><i class="fa fa-angle-down"></i></span>
+						</a>
+						<ul>
+							<li>
+								<a href="teacherDashboard.php" class="ttr-material-button"><span class="ttr-label">Teacher Dashboard</span></a>
+							</li>
+							<li>
+								<a href="teacher-profile.php" class="ttr-material-button"><span class="ttr-label">Teacher Profile</span></a>
+							</li>
+						</ul>
+					</li>
+					<li class="ttr-seperate"></li>
 				</ul>
 				<!-- sidebar menu end -->
 			</nav>
@@ -311,21 +345,21 @@
 					<li><a href="#"><i class="fa fa-home"></i>Home</a></li>
 					<li>Dashboard</li>
 				</ul>
-			</div>	
+			</div>
 			<!-- Card -->
 			<div class="row">
 				<div class="col-md-6 col-lg-3 col-xl-3 col-sm-6 col-12">
-					<div class="widget-card widget-bg1">					 
+					<div class="widget-card widget-bg1">
 						<div class="wc-item">
-							<h4 class="wc-title">
-								Total Frofit
-							</h4>
+							<!-- <h4 class="wc-title">
+								Total Profit
+							</h4> -->
 							<span class="wc-des">
 								All Customs Value
 							</span>
 							<span class="wc-stats">
-								$<span class="counter">18</span>M 
-							</span>		
+								$<span class="counter"><?php echo $totalPrice ?></span>
+							</span>
 							<div class="progress wc-progress">
 								<div class="progress-bar" role="progressbar" style="width: 78%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
 							</div>
@@ -337,21 +371,21 @@
 									78%
 								</span>
 							</span>
-						</div>				      
+						</div>
 					</div>
 				</div>
 				<div class="col-md-6 col-lg-3 col-xl-3 col-sm-6 col-12">
-					<div class="widget-card widget-bg2">					 
+					<div class="widget-card widget-bg2">
 						<div class="wc-item">
 							<h4 class="wc-title">
-								 New Feedbacks
+								New Feedbacks
 							</h4>
 							<span class="wc-des">
 								Customer Review
 							</span>
 							<span class="wc-stats counter">
-								120 
-							</span>		
+								120
+							</span>
 							<div class="progress wc-progress">
 								<div class="progress-bar" role="progressbar" style="width: 88%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
 							</div>
@@ -363,21 +397,21 @@
 									88%
 								</span>
 							</span>
-						</div>				      
+						</div>
 					</div>
 				</div>
 				<div class="col-md-6 col-lg-3 col-xl-3 col-sm-6 col-12">
-					<div class="widget-card widget-bg3">					 
+					<div class="widget-card widget-bg3">
 						<div class="wc-item">
 							<h4 class="wc-title">
-								New Orders 
+								New Orders
 							</h4>
 							<span class="wc-des">
-								Fresh Order Amount 
+								Fresh Order Amount
 							</span>
 							<span class="wc-stats counter">
-								772 
-							</span>		
+								<?php echo $totalteacher ?>
+							</span>
 							<div class="progress wc-progress">
 								<div class="progress-bar" role="progressbar" style="width: 65%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
 							</div>
@@ -389,21 +423,21 @@
 									65%
 								</span>
 							</span>
-						</div>				      
+						</div>
 					</div>
 				</div>
 				<div class="col-md-6 col-lg-3 col-xl-3 col-sm-6 col-12">
-					<div class="widget-card widget-bg4">					 
+					<div class="widget-card widget-bg4">
 						<div class="wc-item">
 							<h4 class="wc-title">
-								New Users 
+								New Users
 							</h4>
 							<span class="wc-des">
 								Joined New User
 							</span>
 							<span class="wc-stats counter">
-								350 
-							</span>		
+								<?php echo $totalusers ?>
+							</span>
 							<div class="progress wc-progress">
 								<div class="progress-bar" role="progressbar" style="width: 90%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
 							</div>
@@ -415,7 +449,7 @@
 									90%
 								</span>
 							</span>
-						</div>				      
+						</div>
 					</div>
 				</div>
 			</div>
@@ -516,7 +550,7 @@
 								<ul>
 									<li>
 										<span class="new-users-pic">
-											<img src="assets/images/testimonials/pic1.jpg" alt=""/>
+											<img src="assets/images/testimonials/pic1.jpg" alt="" />
 										</span>
 										<span class="new-users-text">
 											<a href="#" class="new-users-name">Anna Strong </a>
@@ -528,7 +562,7 @@
 									</li>
 									<li>
 										<span class="new-users-pic">
-											<img src="assets/images/testimonials/pic2.jpg" alt=""/>
+											<img src="assets/images/testimonials/pic2.jpg" alt="" />
 										</span>
 										<span class="new-users-text">
 											<a href="#" class="new-users-name"> Milano Esco </a>
@@ -540,10 +574,10 @@
 									</li>
 									<li>
 										<span class="new-users-pic">
-											<img src="assets/images/testimonials/pic1.jpg" alt=""/>
+											<img src="assets/images/testimonials/pic1.jpg" alt="" />
 										</span>
 										<span class="new-users-text">
-											<a href="#" class="new-users-name">Nick Bold  </a>
+											<a href="#" class="new-users-name">Nick Bold </a>
 											<span class="new-users-info">Web Developer, Facebook Inc </span>
 										</span>
 										<span class="new-users-btn">
@@ -552,7 +586,7 @@
 									</li>
 									<li>
 										<span class="new-users-pic">
-											<img src="assets/images/testimonials/pic2.jpg" alt=""/>
+											<img src="assets/images/testimonials/pic2.jpg" alt="" />
 										</span>
 										<span class="new-users-text">
 											<a href="#" class="new-users-name">Wiltor Delton </a>
@@ -564,11 +598,11 @@
 									</li>
 									<li>
 										<span class="new-users-pic">
-											<img src="assets/images/testimonials/pic3.jpg" alt=""/>
+											<img src="assets/images/testimonials/pic3.jpg" alt="" />
 										</span>
 										<span class="new-users-text">
 											<a href="#" class="new-users-name">Nick Stone </a>
-											<span class="new-users-info">Project Manager, Amazon Inc  </span>
+											<span class="new-users-info">Project Manager, Amazon Inc </span>
 										</span>
 										<span class="new-users-btn">
 											<a href="#" class="btn button-sm outline">Follow</a>
@@ -652,106 +686,105 @@
 	</main>
 	<div class="ttr-overlay"></div>
 
-<!-- External JavaScripts -->
-<script src="assets/js/jquery.min.js"></script>
-<script src="assets/vendors/bootstrap/js/popper.min.js"></script>
-<script src="assets/vendors/bootstrap/js/bootstrap.min.js"></script>
-<script src="assets/vendors/bootstrap-select/bootstrap-select.min.js"></script>
-<script src="assets/vendors/bootstrap-touchspin/jquery.bootstrap-touchspin.js"></script>
-<script src="assets/vendors/magnific-popup/magnific-popup.js"></script>
-<script src="assets/vendors/counter/waypoints-min.js"></script>
-<script src="assets/vendors/counter/counterup.min.js"></script>
-<script src="assets/vendors/imagesloaded/imagesloaded.js"></script>
-<script src="assets/vendors/masonry/masonry.js"></script>
-<script src="assets/vendors/masonry/filter.js"></script>
-<script src="assets/vendors/owl-carousel/owl.carousel.js"></script>
-<script src='assets/vendors/scroll/scrollbar.min.js'></script>
-<script src="assets/js/functions.js"></script>
-<script src="assets/vendors/chart/chart.min.js"></script>
-<script src="assets/js/admin.js"></script>
-<script src='assets/vendors/calendar/moment.min.js'></script>
-<script src='assets/vendors/calendar/fullcalendar.js'></script>
-<script src='assets/vendors/switcher/switcher.js'></script>
-<script>
-  $(document).ready(function() {
+	<!-- External JavaScripts -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/vendors/bootstrap/js/popper.min.js"></script>
+	<script src="assets/vendors/bootstrap/js/bootstrap.min.js"></script>
+	<script src="assets/vendors/bootstrap-select/bootstrap-select.min.js"></script>
+	<script src="assets/vendors/bootstrap-touchspin/jquery.bootstrap-touchspin.js"></script>
+	<script src="assets/vendors/magnific-popup/magnific-popup.js"></script>
+	<script src="assets/vendors/counter/waypoints-min.js"></script>
+	<script src="assets/vendors/counter/counterup.min.js"></script>
+	<script src="assets/vendors/imagesloaded/imagesloaded.js"></script>
+	<script src="assets/vendors/masonry/masonry.js"></script>
+	<script src="assets/vendors/masonry/filter.js"></script>
+	<script src="assets/vendors/owl-carousel/owl.carousel.js"></script>
+	<script src='assets/vendors/scroll/scrollbar.min.js'></script>
+	<script src="assets/js/functions.js"></script>
+	<script src="assets/vendors/chart/chart.min.js"></script>
+	<script src="assets/js/admin.js"></script>
+	<script src='assets/vendors/calendar/moment.min.js'></script>
+	<script src='assets/vendors/calendar/fullcalendar.js'></script>
+	<script src='assets/vendors/switcher/switcher.js'></script>
+	<script>
+		$(document).ready(function() {
 
-    $('#calendar').fullCalendar({
-      header: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'month,agendaWeek,agendaDay,listWeek'
-      },
-      defaultDate: '2019-03-12',
-      navLinks: true, // can click day/week names to navigate views
+			$('#calendar').fullCalendar({
+				header: {
+					left: 'prev,next today',
+					center: 'title',
+					right: 'month,agendaWeek,agendaDay,listWeek'
+				},
+				defaultDate: '2019-03-12',
+				navLinks: true, // can click day/week names to navigate views
 
-      weekNumbers: true,
-      weekNumbersWithinDays: true,
-      weekNumberCalculation: 'ISO',
+				weekNumbers: true,
+				weekNumbersWithinDays: true,
+				weekNumberCalculation: 'ISO',
 
-      editable: true,
-      eventLimit: true, // allow "more" link when too many events
-      events: [
-        {
-          title: 'All Day Event',
-          start: '2019-03-01'
-        },
-        {
-          title: 'Long Event',
-          start: '2019-03-07',
-          end: '2019-03-10'
-        },
-        {
-          id: 999,
-          title: 'Repeating Event',
-          start: '2019-03-09T16:00:00'
-        },
-        {
-          id: 999,
-          title: 'Repeating Event',
-          start: '2019-03-16T16:00:00'
-        },
-        {
-          title: 'Conference',
-          start: '2019-03-11',
-          end: '2019-03-13'
-        },
-        {
-          title: 'Meeting',
-          start: '2019-03-12T10:30:00',
-          end: '2019-03-12T12:30:00'
-        },
-        {
-          title: 'Lunch',
-          start: '2019-03-12T12:00:00'
-        },
-        {
-          title: 'Meeting',
-          start: '2019-03-12T14:30:00'
-        },
-        {
-          title: 'Happy Hour',
-          start: '2019-03-12T17:30:00'
-        },
-        {
-          title: 'Dinner',
-          start: '2019-03-12T20:00:00'
-        },
-        {
-          title: 'Birthday Party',
-          start: '2019-03-13T07:00:00'
-        },
-        {
-          title: 'Click for Google',
-          url: 'http://google.com/',
-          start: '2019-03-28'
-        }
-      ]
-    });
+				editable: true,
+				eventLimit: true, // allow "more" link when too many events
+				events: [{
+						title: 'All Day Event',
+						start: '2019-03-01'
+					},
+					{
+						title: 'Long Event',
+						start: '2019-03-07',
+						end: '2019-03-10'
+					},
+					{
+						id: 999,
+						title: 'Repeating Event',
+						start: '2019-03-09T16:00:00'
+					},
+					{
+						id: 999,
+						title: 'Repeating Event',
+						start: '2019-03-16T16:00:00'
+					},
+					{
+						title: 'Conference',
+						start: '2019-03-11',
+						end: '2019-03-13'
+					},
+					{
+						title: 'Meeting',
+						start: '2019-03-12T10:30:00',
+						end: '2019-03-12T12:30:00'
+					},
+					{
+						title: 'Lunch',
+						start: '2019-03-12T12:00:00'
+					},
+					{
+						title: 'Meeting',
+						start: '2019-03-12T14:30:00'
+					},
+					{
+						title: 'Happy Hour',
+						start: '2019-03-12T17:30:00'
+					},
+					{
+						title: 'Dinner',
+						start: '2019-03-12T20:00:00'
+					},
+					{
+						title: 'Birthday Party',
+						start: '2019-03-13T07:00:00'
+					},
+					{
+						title: 'Click for Google',
+						url: 'http://google.com/',
+						start: '2019-03-28'
+					}
+				]
+			});
 
-  });
-
-</script>
+		});
+	</script>
 </body>
 
 <!-- Mirrored from educhamp.themetrades.com/demo/admin/index.php by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 22 Feb 2019 13:09:05 GMT -->
+
 </html>
